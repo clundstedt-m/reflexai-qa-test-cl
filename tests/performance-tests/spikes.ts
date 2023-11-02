f64e7023654823401dcc073218e78f6d616e2b82
Christian Lundstedt <clundstedtm@gmail.com>
2023-11-02 13:24:18 -0300

    Added Test plan link at the references section

--- tests/performance-tests/spikes.ts
+++ tests/performance-tests/spikes.ts
@@ -3,9 +3,9 @@ import http from 'k6/http';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 10 },    // Steady load of 10 virtual users for 1 minute
-    { duration: '1m', target: 150 },   // Spike to 100 virtual users for 1 minute
-    { duration: '20s', target: 10 },    // Back to 10 virtual users for 1 minute
+    { duration: '30s', target: 10 },    // Steady load of 10 virtual users for 30 seconds
+    { duration: '1m', target: 150 },   // Spike to 150 virtual users for 1 minute
+    { duration: '20s', target: 10 },    // Back to 10 virtual users for 20 seconds
   ],
 };
 
